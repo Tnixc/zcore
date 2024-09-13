813781700da0b6b31034e314cf70403146ec1a9c
Tnixc <tnixxc@gmail.com>
2024-09-13 11:41:32 -0400

    what

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-
+const read = @import("read.zig");
 pub const Op = enum { load, store, ALUadd, ALUsub, ALUand, ALUor, ALUnot, jump, jumpz, halt, IOin, IOout };
 
 pub const Instruction = struct {
@@ -19,21 +19,22 @@ pub const Cpu = struct {
 };
 
 pub fn main() !void {
-    const cpu = Cpu{
-        .PC = 0,
-        .IR = 0,
-        .Flags = 0b0000_0000,
-        .Registers = [_]u16{0} ** 8,
-        .WorkingMemory = [_]u16{0} ** 256,
-        .ProgramMemory = [_]u16{0} ** 256,
-    };
-    std.debug.print("CPU!\n{any}\n", .{cpu});
-
-    const sample = Instruction{
-        .op = Op.load,
-        .dest = 0,
-        .src1 = 0,
-        .src2 = 0,
-    };
-    std.debug.print("Sample instruction: {any}\n", .{sample});
+    try read.readASM("foo.txt");
+    // const cpu = Cpu{
+    //     .PC = 0,
+    //     .IR = 0,
+    //     .Flags = 0b0000_0000,
+    //     .Registers = [_]u16{0} ** 8,
+    //     .WorkingMemory = [_]u16{0} ** 256,
+    //     .ProgramMemory = [_]u16{0} ** 256,
+    // };
+    // std.debug.print("CPU!\n{any}\n", .{cpu});
+    //
+    // const sample = Instruction{
+    // .op = Op.load,
+    //     .dest = 0,
+    //     .src1 = 0,
+    //     .src2 = 0,
+    // };
+    // std.debug.print("Sample instruction: {any}\n", .{sample});
 }
